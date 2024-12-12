a761cc047dbdc4986ec38f28d3999c9bf0be780f
jeroen1010 <jjaspez92@ieszaidinvergeles.org>
2024-12-13 00:31:28 +0100

    Creación y modificación de App.tsx (los cambios en los paquetes son debido a los 100 comandos install diferentes que he puesto pq no me lanza el emulador, me da un error react-native-reanimated)

--- App.tsx
+++ App.tsx
@@ -1,8 +1,22 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
-import Navigator from './src/navigation/Navigator';
+import ContinentScreen from './src/screens/ContinentScreen';
+import CountryScreen from './src/screens/CountryScreen';
+import CountryDetailsScreen from './src/screens/CountryDetailsScreen';
+import { Provider } from 'react-redux';
 
-const App = () => {
-  return <Navigator />;
-};
+const Stack = createNativeStackNavigator();
 
-export default App;
+export default function App(): React.JSX.Element {
+    return (
+        
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Continent" component={ContinentScreen} />
+                <Stack.Screen name="Country" component={CountryScreen} />
+            </Stack.Navigator>a
+        </NavigationContainer>
+        
+    );
+}
